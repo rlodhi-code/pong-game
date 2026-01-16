cdc08c53f5d4dc7f18eaab151659ee1a26a379c9
User 1 <rlodhicode@gmail.com>
2026-01-16 14:24:29 -0600

    created a new class paddle.py and adjusted the code.

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0) # turns off the animation
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-    screen.update()
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-    screen.update()
-
-
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
 screen.listen()
-screen.update()
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
+
